1ef9fc47d19b823a93e0203f47348247e4b07985
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-18 17:13:58 +0100

    init end

--- src/server.js
+++ src/server.js
@@ -1,2 +1,20 @@
-'use strict';
-console.log('server.js file was loaded');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// listen
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
