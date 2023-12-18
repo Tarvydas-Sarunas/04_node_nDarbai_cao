688ee756f63645c95a29654d9fae200e7f3fe9a1
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-18 17:47:07 +0100

    cars models

--- src/server.js
+++ src/server.js
@@ -9,12 +9,30 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
+// DATA
+const cars = {
+  bmw: ['i3', 'i8', '1 series', '3 series', '5 series'],
+  mb: ['A class', 'C class', 'E class', 'S class'],
+  vw: ['Golf', 'Arteon', 'UP'],
+};
+
 // routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// app.get/api/cars paduoda visus cars
+app.get('/api/cars', (req, res) => {
+  res.json(cars);
+});
+// app.get/api/cars paduoda visus cars
+app.get('/api/cars/:brand', (req, res) => {
+  const brand = req.params.brand;
+  const allModels = cars[brand];
+  res.json(allModels);
+});
+
 // listen
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
