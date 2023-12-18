ab90b8ab55f1eec24bc0a6e242277b1f9158a8fc
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-18 19:37:28 +0100

    end namu darnbai cao

--- src/server.js
+++ src/server.js
@@ -16,15 +16,67 @@ const cars = {
   vw: ['Golf', 'Arteon', 'UP'],
 };
 
+const { data } = require('./db/data');
+
 // routes
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// ROUTES DATA
+// get /api/users paduos visus users
+app.get('/api/users', (req, res) => {
+  res.json(data);
+});
+// get /api/emails/ paduos visus emailus
+app.get('/api/emails', (req, res) => {
+  const emailai = data.map((arrObj) => arrObj.email);
+  res.json(emailai);
+});
+
+// get /api/femaleNames/
+app.get('/api/femaleNames', (req, res) => {
+  const famale = 'Female';
+  const famales = data.filter((arrObj) => arrObj.gender === famale);
+  const famalesNames = famales.map(
+    (arrObj) => `${arrObj.first_name} ${arrObj.last_name}`
+  );
+  res.json(famalesNames);
+});
+
+// get /api/users/:userId paduos visus users
+app.get('/api/users/:userId', (req, res) => {
+  const userId = +req.params.userId;
+  const found = data.find((arrObj) => arrObj.id === userId);
+  if (found === undefined) {
+    res.status(404).json({
+      msg: `user not found with id ${userId}`,
+    });
+    return;
+  }
+  res.json(found);
+});
+
+// get /api/users/:brand paduos visus users
+app.get('/api/users/:brand', (req, res) => {
+  const brand = req.params.brand;
+  const found = data.filter((arrObj) => arrObj.car === brand);
+  if (found === undefined) {
+    res.status(404).json({
+      msg: `user not found with car ${brand}`,
+    });
+    return;
+  }
+  res.json(found);
+});
+
+// PIRMA DALIS
+
 // app.get/api/cars paduoda visus cars
 app.get('/api/cars', (req, res) => {
   res.json(cars);
 });
+
 // app.get/api/cars paduoda visus cars
 app.get('/api/cars/:brand', (req, res) => {
   const brand = req.params.brand;
